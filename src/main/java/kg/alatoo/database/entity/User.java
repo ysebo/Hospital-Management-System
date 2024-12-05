de6fb12c3a5a53adecd40e4e27368e0c07c6b5b3
ysebo <asangulovtimur6@gmail.com>
2024-12-05 21:03:30 +0100

    added user info with implementing userDetails

--- src/main/java/kg/alatoo/database/entity/User.java
+++ src/main/java/kg/alatoo/database/entity/User.java
@@ -1,4 +1,61 @@
 package kg.alatoo.database.entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-public class User {
+import java.util.Collection;
+import java.util.List;
+
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String password;
+    private String phone;
+
+
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+
 }
