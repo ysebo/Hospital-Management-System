b8765ff994118f0d85a405ddc1be0fabc9ed38d9
ysebo <asangulovtimur6@gmail.com>
2024-12-05 20:44:06 +0100

    connected database for project and added secret.properties

--- src/main/java/kg/alatoo/database/controller/AuthController.java
+++ src/main/java/kg/alatoo/database/controller/AuthController.java
@@ -1,10 +1,12 @@
 package kg.alatoo.database.controller;
 
 import kg.alatoo.database.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/v1/auth")
 public class AuthController {
     private AuthService authService;
